76e680d7aa0c4a6098647a793fb2e68087f0ae99
Victor3241 <103729389+Victor3241@users.noreply.github.com>
2023-07-11 14:40:49 +0300

    OK

---
+++ SDL_TriangleRender/SDL_TriangleRender/rasterization.cpp
@@ -0,0 +1,18 @@
+#include "rasterization.h"
+
+namespace egc {
+
+
+	void computeAlphaBetaGamma(const std::vector<egc::vec4>& triangleVertices, vec2 pixel, float& alpha, float& beta, float& gamma) 
+	{
+		//TO DO - Compute alfa, beta and gamma => we use the function's input parameters as the return mechanism
+		//Store the final results in the input parameters
+	}
+
+	void rasterizeTriangle(SDL_Renderer *renderer, const std::vector<egc::vec4> &triangleVertices, const std::vector<egc::vec4> &triangleColors) {
+		//TO DO - Implement the triangle rasterization algorithm
+
+
+	}
+
+}
